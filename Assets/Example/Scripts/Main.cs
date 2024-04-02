d929bdeba7db8026b6554de4c5eb24364e76b78d
ADM87 <40219687+ADM87@users.noreply.github.com>
2024-04-02 09:09:31 -0300

    Update Main.cs

--- Assets/Example/Scripts/Main.cs
+++ Assets/Example/Scripts/Main.cs
@@ -7,16 +7,18 @@ namespace ADM.Example
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
         private static void Boot()
         {
-            // Collection definitions with the ServiceDefinition attribute.
-            ServiceProvider.CollectDefinitions();
-
-            // Register the example message type. This will create a IMessenger<T>
+            // Registers the example message type. This will create a IMessenger<T>
             // service definition with the service provider that will allow 
             // sending and receiving messages of type ExampleMessage.
+            // Flagging the message type as a singleton will ensure the IMessenger<T>
+            // is the same instances anytime it is accessed from the ServiceProvider.
             MessageRegistry.RegisterMessageType<ExampleMessage>(isSingleton: true);
 
+            // Collection definitions with the ServiceDefinition attribute.
+            ServiceProvider.CollectDefinitions();
+            
             // After all other definition registration is complete, 
-            // we construst all singleton service instances.
+            // we construst all service flagged as singletons.
             //
             // Note: This is optional depending on how you want to structure you systems.
             //       This ensures that any singleton services are ready to use before the
